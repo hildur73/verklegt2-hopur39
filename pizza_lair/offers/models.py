d2fd99169abb02156d68400ccf367b0045ad6a5d
helgafrimanns <112622952+helgafrimanns@users.noreply.github.com>
2023-05-03 10:58:08 +0000

    commit

--- pizza_lair/offers/models.py
+++ pizza_lair/offers/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from menu.models import Menu
 # Create your models here.
 class Offers(models.Model):
-    #vantar id
     name = models.CharField(max_length=255)
     price = models.FloatField()
-    #fær líka id frá Menu
+    pizza_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
