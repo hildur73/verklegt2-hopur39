6c5d114a252ae908a868fa581430da8e1b277eb3
helgafrimanns <112622952+helgafrimanns@users.noreply.github.com>
2023-05-03 11:21:30 +0000

    push

--- pizza_lair/menu/models.py
+++ pizza_lair/menu/models.py
@@ -5,13 +5,9 @@ from django.db import models
 class Menu(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
-
-
-class Menu_details(models.Model):
-    description = models.CharField(max_length=255, blank=True)
-    pizza_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
-
-
-class Menu_image(models.Model):
-    pizza_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
     image = models.CharField(max_length=9999)
+
+class Menudetails(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+    menuid = models.ForeignKey(Menu, on_delete=models.CASCADE)
+
