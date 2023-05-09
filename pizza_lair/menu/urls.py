441a7069d0629bf68b9fdcbe2b12b784fcbcd396
helgafrimanns <112622952+helgafrimanns@users.noreply.github.com>
2023-05-09 08:35:05 +0000

    hvað? eins og þetta sé erfitt?

--- pizza_lair/menu/urls.py
+++ pizza_lair/menu/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('<int:id>', views.get_pizza_by_id, name='menu_details'),
     path('create_menu', views.create_menu, name='create_menu'),
     path('delete_menu/<int:id>', views.delete_menu, name='delete_menu'),
-    path('update_menu/<int:id>', views.update_menu, name='update_menu')
-
+    path('update_menu/<int:id>', views.update_menu, name='update_menu'),
 ]
